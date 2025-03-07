4fcd751d7a06e32f59ac8d14e145c1f8f3f8c7e1
Leon <mail@leon-schoenberg.de>
2025-03-07 14:49:33 +0100

    Fix problem with all empty report scenarios

--- WeBBAKI/src/main/java/de/thb/webbaki/service/snapshot/ReportService.java
+++ WeBBAKI/src/main/java/de/thb/webbaki/service/snapshot/ReportService.java
@@ -32,7 +32,7 @@ import java.util.stream.Collectors;
 
 
 @Service
-public class  ReportService {
+public class ReportService {
 
     @Autowired
     private UserService userService;
@@ -45,10 +45,21 @@ public class  ReportService {
     @Autowired
     private ScenarioService scenarioService;
 
-    public Report getCompanyReport(Snapshot snapshot, String username){return reportRepository.findBySnapshotAndUser_Username(snapshot, username);}
-    public Report getBranchReport(Snapshot snapshot, Branch branch){return reportRepository.findBySnapshotAndBranch(snapshot, branch);}
-    public Report getSectorReport(Snapshot snapshot, Sector sector){return reportRepository.findBySnapshotAndSector(snapshot, sector);}
-    public Report getNationalReport(Snapshot snapshot){return reportRepository.findBySnapshotAndUserIsNullAndBranchIsNullAndSectorIsNull(snapshot);}
+    public Report getCompanyReport(Snapshot snapshot, String username) {
+        return reportRepository.findBySnapshotAndUser_Username(snapshot, username);
+    }
+
+    public Report getBranchReport(Snapshot snapshot, Branch branch) {
+        return reportRepository.findBySnapshotAndBranch(snapshot, branch);
+    }
+
+    public Report getSectorReport(Snapshot snapshot, Sector sector) {
+        return reportRepository.findBySnapshotAndSector(snapshot, sector);
+    }
+
+    public Report getNationalReport(Snapshot snapshot) {
+        return reportRepository.findBySnapshotAndUserIsNullAndBranchIsNullAndSectorIsNull(snapshot);
+    }
 
     public int getNumberOfSectorsInSnap(Snapshot snap) {
         List<Report> reportsOfSnap = snap.getReports();
@@ -69,7 +80,7 @@ public class  ReportService {
         List<Report> reportsOfSnap = snap.getReports();
         Set<Branch> uniqueBranches = new HashSet<>();
 
-        if(sector != null) {
+        if (sector != null) {
             for (Report report : reportsOfSnap) {
                 Branch branch = report.getBranch();
                 if (!uniqueBranches.contains(branch) && sector.getBranches().contains(branch) && branch != null) {
@@ -89,9 +100,10 @@ public class  ReportService {
 
     /**
      * Creates all Report types (company, branch, sector and national)
+     *
      * @param snapshot
      */
-    public void createReports(Snapshot snapshot){
+    public void createReports(Snapshot snapshot) {
         //A map mapped by the Branch. The value is another map with the Scenario as Key. The value is a List of ReportScenarios (important for calculating the average)
         Map<Branch, Map<Scenario, List<ReportScenario>>> branchMapOftReportScenarioListMaps = new HashMap<>();
         //A map mapped by the sector. The value is another map with the Scenario as Key. The value is a List of ReportScenarios (important for calculating the average)
@@ -107,6 +119,7 @@ public class  ReportService {
 
     /**
      * creates the national report and the average ReportScenarios from scenarioMapOfBranchAverageReportScenarios
+     *
      * @param snapshot
      * @param scenarioMapOfBranchAverageReportScenarios
      * @param numberOfQuestionnaires
@@ -130,6 +143,7 @@ public class  ReportService {
 
     /**
      * creates all sector reports and calculates the average ReportScenarios from given sectorMapOftReportScenarioListMaps
+     *
      * @param snapshot
      * @param sectorMapOftReportScenarioListMaps
      */
@@ -159,10 +173,11 @@ public class  ReportService {
 
     /**
      * creates all branch reports and calculates the average ReportScenarios from given branchMapOftReportScenarioListMap
+     *
      * @param snapshot
      * @param branchMapOftReportScenarioListMaps
-     * @param scenarioMapOfBranchAverageReportScenarios  saves the branch averages in this map for the later
-     *                                                   calculation of the national report
+     * @param scenarioMapOfBranchAverageReportScenarios saves the branch averages in this map for the later
+     *                                                  calculation of the national report
      */
     private void createBranchReports(Snapshot snapshot, Map<Branch, Map<Scenario, List<ReportScenario>>> branchMapOftReportScenarioListMaps, Map<Scenario, List<ReportScenario>> scenarioMapOfBranchAverageReportScenarios) {
         //List for the branch Average ReportScenarios
@@ -183,7 +198,7 @@ public class  ReportService {
                 branchReportScenario.setScenario(scenario);
 
                 //create the list if not exists by key
-                if(!scenarioMapOfBranchAverageReportScenarios.containsKey(scenario)){
+                if (!scenarioMapOfBranchAverageReportScenarios.containsKey(scenario)) {
                     scenarioMapOfBranchAverageReportScenarios.put(scenario, new ArrayList<>());
                 }
                 //add the average ReportScenario of this Branch to the List mapped by the current Scenario
@@ -198,80 +213,92 @@ public class  ReportService {
 
     /**
      * Creates the company reports and their ReportScenarios
+     *
      * @param snapshot
      * @param branchMapOftReportScenarioListMaps A map mapped by the Branch. The value is another map with the Scenario as Key. And the value of this is a List of ReportScenarios
      * @param sectorMapOftReportScenarioListMaps A map mapped by the sector. The value is another map with the Scenario as Key. And the value of this is a List of ReportScenarios
-     * fill both Maps with the right ReportScenarios for the later calculation of branch and sector reports
+     *                                           fill both Maps with the right ReportScenarios for the later calculation of branch and sector reports
      */
     @Transactional
     public void createCompanyReports(Snapshot snapshot, Map<Branch, Map<Scenario, List<ReportScenario>>> branchMapOftReportScenarioListMaps, Map<Sector, Map<Scenario, List<ReportScenario>>> sectorMapOftReportScenarioListMaps) {
         //get all scenarios for the calculation of the reportScenarios
         List<Scenario> scenarios = scenarioService.getAllScenarios();
 
-        for (User user : userService.getAllUsers()){
+        for (User user : userService.getAllUsers()) {
             //only use quest of a user if this user is a KRITIS_BETREIBER and the user is enabled
-            if(user.isEnabled() && userService.existsUserByIdAndRoleName(user.getId(), "ROLE_KRITIS_BETREIBER")) {
-                //get last created questionnaire (automatically the last one in the list)
-                List<Questionnaire> questionnaires = user.getQuestionnaires();
-                int test = questionnaires.size() - 1;
-                Questionnaire questionnaire = questionnaires.get(questionnaires.size() - 1);
-                if (questionnaire != null) {
-                    //Company part
-                    Report companyReport = Report.builder().snapshot(snapshot).user(user).numberOfQuestionnaires(1).comment(questionnaire.getComment()).build();
-                    reportRepository.save(companyReport);
+            if (!(user.isEnabled() && userService.existsUserByIdAndRoleName(user.getId(), "ROLE_KRITIS_BETREIBER")))
+                continue;
 
-                    //calculate all the ReportScenarios from the questionnaires UserScenarios
-                    List<ReportScenario> companyReportScenarios = reportScenarioService.calculateReportScenariosFromUserScenarios(questionnaire.getUserScenarios(), scenarios);
-                    for(ReportScenario reportScenario : companyReportScenarios){
-                        reportScenario.setReport(companyReport);
+            //get last created questionnaire (automatically the last one in the list)
+            List<Questionnaire> questionnaires = user.getQuestionnaires();
 
-                        //Branch part
-                        //create map inside the branch map if not exists
-                        if(!branchMapOftReportScenarioListMaps.containsKey(user.getBranch())){
-                            branchMapOftReportScenarioListMaps.put(user.getBranch(), new HashMap<Scenario, List<ReportScenario>>());
-                        }
-                        //create list inside the scenario map inside the branch map if not exists
-                        if(!branchMapOftReportScenarioListMaps.get(user.getBranch()).containsKey(reportScenario.getScenario())){
-                            branchMapOftReportScenarioListMaps.get(user.getBranch()).put(reportScenario.getScenario(), new ArrayList<>());
-                        }
-                        //now add the reportScenario to the right list
-                        branchMapOftReportScenarioListMaps.get(user.getBranch()).get(reportScenario.getScenario()).add(reportScenario);
+            Questionnaire questionnaire = questionnaires.get(questionnaires.size() - 1);
+            if (questionnaire == null)
+                continue;
 
-                        //Sector part
-                        //create map inside the sector map if not exists
-                        if(!sectorMapOftReportScenarioListMaps.containsKey(user.getBranch().getSector())){
-                            sectorMapOftReportScenarioListMaps.put(user.getBranch().getSector(), new HashMap<Scenario, List<ReportScenario>>());
-                        }
-                        //create list inside the scenario map inside the sector map if not exists
-                        if(!sectorMapOftReportScenarioListMaps.get(user.getBranch().getSector()).containsKey(reportScenario.getScenario())){
-                            sectorMapOftReportScenarioListMaps.get(user.getBranch().getSector()).put(reportScenario.getScenario(), new ArrayList<>());
-                        }
-                        //now add the reportScenario to the right list
-                        sectorMapOftReportScenarioListMaps.get(user.getBranch().getSector()).get(reportScenario.getScenario()).add(reportScenario);
-                    }
-                    //save all CompanyReportScenarios
-                    reportScenarioService.saveAllReportScenarios(companyReportScenarios);
+            //calculate all the ReportScenarios from the questionnaires UserScenarios
+            List<ReportScenario> companyReportScenarios = reportScenarioService.calculateReportScenariosFromUserScenarios(questionnaire.getUserScenarios(), scenarios);
 
+            // only create process if not all are empty
+            if(companyReportScenarios.stream().mapToInt(ReportScenario::getNumberOfValues).sum() <= 0)
+                continue;
+
+            //Company part
+            Report companyReport = Report.builder().snapshot(snapshot).user(user).numberOfQuestionnaires(1).comment(questionnaire.getComment()).build();
+            reportRepository.save(companyReport);
+
+            for (ReportScenario reportScenario : companyReportScenarios) {
+                reportScenario.setReport(companyReport);
+
+                //Branch part
+                //create map inside the branch map if not exists
+                if (!branchMapOftReportScenarioListMaps.containsKey(user.getBranch())) {
+                    branchMapOftReportScenarioListMaps.put(user.getBranch(), new HashMap<Scenario, List<ReportScenario>>());
                 }
+                //create list inside the scenario map inside the branch map if not exists
+                if (!branchMapOftReportScenarioListMaps.get(user.getBranch()).containsKey(reportScenario.getScenario())) {
+                    branchMapOftReportScenarioListMaps.get(user.getBranch()).put(reportScenario.getScenario(), new ArrayList<>());
+                }
+                //now add the reportScenario to the right list
+                branchMapOftReportScenarioListMaps.get(user.getBranch()).get(reportScenario.getScenario()).add(reportScenario);
+
+                //Sector part
+                //create map inside the sector map if not exists
+                if (!sectorMapOftReportScenarioListMaps.containsKey(user.getBranch().getSector())) {
+                    sectorMapOftReportScenarioListMaps.put(user.getBranch().getSector(), new HashMap<Scenario, List<ReportScenario>>());
+                }
+                //create list inside the scenario map inside the sector map if not exists
+                if (!sectorMapOftReportScenarioListMaps.get(user.getBranch().getSector()).containsKey(reportScenario.getScenario())) {
+                    sectorMapOftReportScenarioListMaps.get(user.getBranch().getSector()).put(reportScenario.getScenario(), new ArrayList<>());
+                }
+                //now add the reportScenario to the right list
+                sectorMapOftReportScenarioListMaps.get(user.getBranch().getSector()).get(reportScenario.getScenario()).add(reportScenario);
             }
+            //save all CompanyReportScenarios
+            reportScenarioService.saveAllReportScenarios(companyReportScenarios);
+
         }
+
+
     }
 
     /**
      * Returns the right report from DB based.
      * The decision is based on given ReportFocus
+     *
      * @param reportFocus
      * @param username
      * @param snapshot
      * @return
      * @throws UnknownReportFocusException
      */
-    public MappingReport getMappingReportByReportFocus(ReportFocus reportFocus, String username, Snapshot snapshot) throws UnknownReportFocusException {
+    public MappingReport getMappingReportByReportFocus(ReportFocus reportFocus, String username, Snapshot snapshot) throws
+            UnknownReportFocusException {
         User user = userService.getUserByUsername(username);
         Report report;
         switch (reportFocus) {
             case COMPANY:
-                report = getCompanyReport(snapshot,username);
+                report = getCompanyReport(snapshot, username);
                 break;
             case BRANCHE:
                 report = getBranchReport(snapshot, user.getBranch());
@@ -286,21 +313,20 @@ public class  ReportService {
                 throw new UnknownReportFocusException();
         }
 
-        if(report == null || report.getReportScenarios() == null){
+        if (report == null || report.getReportScenarios() == null) {
             return new MappingReport(Report.builder().numberOfQuestionnaires(0).build());
-        }else{
+        } else {
             return new MappingReport(report);
         }
     }
 
     /**
-     *
      * @param template Does not need the .html.
-     * @param context Contains the variables that we want to be passed to Thymeleaf.
-     *                Addable with context.setVariable(key, value);
+     * @param context  Contains the variables that we want to be passed to Thymeleaf.
+     *                 Addable with context.setVariable(key, value);
      * @return the html of the parsed template code as a String
      */
-    public String parseThymeleafTemplateToHtml(String template, Context context){
+    public String parseThymeleafTemplateToHtml(String template, Context context) {
         ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
         templateResolver.setSuffix(".html");
         templateResolver.setTemplateMode(TemplateMode.HTML);
@@ -313,24 +339,25 @@ public class  ReportService {
 
     /**
      * Create a pdf outputStream from a html-string
+     *
      * @param html
      * @param outputStream is used as return value to write in the pdf-document-stream
-     * @throws IOException
-     * All styles in the html have to be inline.
-     * Not closed elements like <br> and <link> have to be closed like this
-     * <br></br> and <link></link>
+     * @throws IOException All styles in the html have to be inline.
+     *                     Not closed elements like <br> and <link> have to be closed like this
+     *                     <br></br> and <link></link>
      */
-    public void generatePdfFromHtml(String html, String baseUrl, OutputStream outputStream) throws IOException, DocumentException {
+    public void generatePdfFromHtml(String html, String baseUrl, OutputStream outputStream) throws
+            IOException, DocumentException {
         BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(outputStream);
         ITextRenderer renderer = new ITextRenderer();
-        renderer.setDocumentFromString(html,baseUrl);
+        renderer.setDocumentFromString(html, baseUrl);
         renderer.layout();
         renderer.createPDF(bufferedOutputStream);
 
         bufferedOutputStream.close();
     }
 
-    public List<Report> getReportBySnapshotId(Long id){
+    public List<Report> getReportBySnapshotId(Long id) {
 
         return reportRepository.findBySnapshotId(id);
     }
